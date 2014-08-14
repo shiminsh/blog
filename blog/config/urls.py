57cbc0a2403239e13a7bba7bb54d3b606bbc102f
Shalini Roy <shaliniroy012@gmail.com>
2014-08-14 23:29:13 +0530

    Added home page

--- blog/config/urls.py
+++ blog/config/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-from blogging import views
+from django.views.generic import TemplateView
 from django.contrib import admin
+from blogging import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name="home"),
     url(r'^accounts/register/$', views.RegisterUser.as_view()),
     url(r'^accounts/register_success/$', 'blogging.views.register_success'),
 )
